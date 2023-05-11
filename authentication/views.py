5f8d0984dd754d1520f990ab1b8fb3776e5378e0
dima-lkv <ilkiv.dima15@gmail.com>
2023-05-11 05:29:53 +0300

    -auth super

--- authentication/views.py
+++ authentication/views.py
@@ -1,12 +1,13 @@
-from django.contrib.auth.models import User
-from django.shortcuts import render
-from rest_framework import viewsets, permissions
+from django.contrib.auth.models import Group, User
+from rest_framework import permissions, viewsets
 
-from authentication.serializer import UserSerializer
+from .serializers import UserSerializer
 
 
-# Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticated]
