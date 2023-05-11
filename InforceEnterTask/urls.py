60d9ab9542fb2722551ec1fe71b12b2cb2b74ea0
dima-lkv <ilkiv.dima15@gmail.com>
2023-05-11 05:15:32 +0300

    commit 01

--- InforceEnterTask/urls.py
+++ InforceEnterTask/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 from rest_framework import routers
 
 from authentication.views import UserViewSet
+# from restaurant.views import UserViewSet
+
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
