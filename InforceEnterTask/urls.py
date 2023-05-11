60c9023ca8333fe291cc7d5246f5ea4cd4d7112c
dima-lkv <ilkiv.dima15@gmail.com>
2023-05-11 18:11:08 +0300

    todo: employee authentication

--- InforceEnterTask/urls.py
+++ InforceEnterTask/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from rest_framework import routers
 from authentication.views import UserViewSet
 from restaurant.views import RestaurantViewSet
-from menu.views import MenuViewSet, TodayMenuViewSet
+from menu.views import MenuViewSet, TodayMenuViewSet, WinnerMenuViewSet
 from vote.views import VoteViewSet
 
 router = routers.DefaultRouter()
@@ -12,6 +12,7 @@ router.register(r'menu', MenuViewSet, basename='menu')
 router.register(r'vote', VoteViewSet)
 
 router.register(r'today_menu', TodayMenuViewSet, basename='today_menu')
+router.register(r'winner_menu', WinnerMenuViewSet, basename='winner_menu')
 
 urlpatterns = [
     path('', include(router.urls)),
--- menu/views.py
+++ menu/views.py
@@ -1,10 +1,12 @@
 from datetime import datetime
 
+from django.db.models import Count, F
 from rest_framework import viewsets
 
 import custom_permissions
 from menu.serializers import MenuSerializer
 from menu.models import Menu
+from vote.models import Vote
 
 
 class MenuViewSet(viewsets.ModelViewSet):
@@ -18,10 +20,24 @@ class MenuViewSet(viewsets.ModelViewSet):
 
 class TodayMenuViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows today menus to be viewed or edited.
+    API endpoint that allows today menu to be viewed.
     """
     serializer_class = MenuSerializer
     permission_classes = [custom_permissions.GetOnly]
 
     def get_queryset(self):
         return Menu.objects.filter(week_day=datetime.now().weekday()).order_by('restaurant_id')
+
+
+class WinnerMenuViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows today winner menu to be viewed.
+    """
+    serializer_class = MenuSerializer
+    permission_classes = [custom_permissions.GetOnly]
+
+    def get_queryset(self):
+        today_votes = Vote.objects.filter(created_at=datetime.now().strftime("%m/%d/%Y"))
+        votes = today_votes.values('menu_id').annotate(count=Count('menu_id')).order_by('-count')
+        top_vote = votes.first()
+        return Menu.objects.filter(id=top_vote['menu_id'])
